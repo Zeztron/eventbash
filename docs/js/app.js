7c2b62728e93faded36de2878a7f064e6533cdae
Zeztron <pateltharsh@gmail.com>
2018-11-20 20:13:29 -0500

    :rocket:

---
+++ docs/js/app.js
@@ -0,0 +1,35 @@
+// instantiate both classes
+const eventbrite = new EventBrite();
+const ui = new UI();
+
+// create listener for submit button
+document.getElementById("submitBtn").addEventListener("click", (e) => {
+    e.preventDefault();
+
+    // get values from form
+    const eventName = document.getElementById("event-name").value;
+    const category = document.getElementById("category").value;
+    
+    //console.log(eventName + ": " + category); 
+    if(eventName !== "") {
+        // Query Event Brite API
+        eventbrite.queryAPI(eventName, category)
+        .then(events => {
+            // check for events
+            const eventsList = events.events.events;
+            if (eventsList.length > 0) {
+                // print events
+                ui.displayEvents(eventsList);
+                document.getElementById("event-name").value = "";
+            } else {
+                ui.printMessage("No Results Found.", "text-center alert alert-danger mt-4");
+                document.getElementById("event-name").value = "";
+            }
+            
+        })
+
+    } else {
+       // Print a message
+       ui.printMessage("Add an Event or City, Please :)", "text-center alert alert-danger mt-4");
+    }
+});
---
+++ docs/js/eventbrite.js
@@ -0,0 +1,32 @@
+class EventBrite {
+    // Constructor when instantiate
+    constructor() {
+        this.auth_token = "XOCJJPO4X6QEBRLDTLHZ";
+        this.orderby = 'date';
+    }
+
+    // Get the events from API
+    async queryAPI(eventName, category) {
+        const eventsResponse = await fetch(`https://www.eventbriteapi.com/v3/events/search/?q=${eventName}&sort_by=${this.orderby}&categories=${category}&token=${this.auth_token}`);
+
+        // wait for response and return as json
+        const events = await eventsResponse.json();
+        console.log(events);
+        
+        return {
+            events
+        }
+    }
+
+    // get categories from API
+    async getCategoriesAPI() {
+        // query the API
+        const categoriesResponse = await fetch(`https://www.eventbriteapi.com/v3/categories/?token=${this.auth_token}`);
+
+        // hold for the response and then return as json
+        const categories = await categoriesResponse.json();
+        return {
+            categories
+        }
+    }
+}
